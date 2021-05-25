4d48b49f44e22e0a2e9198e5f00884926d4c52e6
Rajvardhan44 <75527625+Rajvardhan44@users.noreply.github.com>
2021-05-25 17:49:23 +0530

    Add files via upload

---
+++ screens/transaction.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Transaction extends Component{
+    render(){
+        return(
+         <View>
+             <Text>Issue or Return</Text>
+         </View>
+        )
+    }
+}
